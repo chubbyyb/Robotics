61c04f2493d8aca6c360cb8e7e156beff5be9d13
Keith Salhani <c22322811@mytudublin.ie>
2023-02-11 22:13:04 +0000

    Challenge1.c Lab3 using Simulator (bump detection)

---
+++ lab3/simulator/Challenge1.c
@@ -0,0 +1,51 @@
+
+int getDistance(int distance) {
+	
+	// Loop until button pressed
+	while (getButtonPress(buttonEnter) == 0 )  // this will fail once the button is enter pressed. 
+	{
+		
+		displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+		
+		if (getButtonPress(buttonUp)) {
+			distance = distance+1;
+			displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+		}
+		else if (getButtonPress(buttonDown)) 
+		{
+			
+			if(distance != 0)
+			{
+				distance = distance-1;
+				displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+			}
+		}
+		
+		// Wait 20 ms, this gives us 50 readings per second
+		sleep(20);
+	}
+	return distance;
+	
+}
+
+void drive(long nMotorRatio, long dist, long power)
+{
+	setMotorSyncEncoder(motorB, motorC, nMotorRatio, dist, power);
+	sleep(10000);
+}
+
+
+task main()
+{
+	// Use this if you want to use the back button as well
+	// Not advisable.
+	//setBlockBackButton(true);
+	
+	int distance = 0;
+	int randomNum = random(100);
+	
+	displayCenteredTextLine(1, "Pressed button:");
+	displayCenteredBigTextLine(6,"Final Dist = %dcm", distance = getDistance(distance));
+	sleep(1000);
+	drive(0,distance * 10,randomNum);
+}
