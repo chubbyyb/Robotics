2f8a338f384b89f6efbf3bac6bffb34dadb2ccf6
Keith Salhani <c22322811@mytudublin.ie>
2023-02-15 12:26:02 +0000

    ButtonEncoderSync.c, Set cm and travel cm

---
+++ lab3/ButtonEncoderSync.c
@@ -0,0 +1,60 @@
+#pragma config(Sensor, S1,     touchSensor,    sensorEV3_Touch)
+#pragma config(Sensor, S2,     gyroSensor,     sensorEV3_Gyro, modeEV3Gyro_RateAndAngle)
+#pragma config(Sensor, S3,     colorSensor,    sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S4,     sonarSensor,    sensorEV3_Ultrasonic)
+#pragma config(Motor,  motorA,          armMotor,      tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorB,          leftMotor,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, driveRight, encoder)
+
+
+
+int getDistance(int distance) {
+	
+	// Loop until button pressed
+	while (getButtonPress(buttonEnter) == 0 )  // this will fail once the button is enter pressed. 
+	{
+		
+		displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+		
+		if (getButtonPress(buttonUp)) {
+			distance = distance+1;
+			displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+		}
+		else if (getButtonPress(buttonDown)) 
+		{
+			
+			if(distance != 0)
+			{
+				distance = distance-1;
+				displayCenteredBigTextLine(4,"Distance = %dcm", distance);
+			}
+		}
+		
+		// Wait 20 ms, this gives us 50 readings per second
+		sleep(20);
+	}
+	return distance;
+	
+}
+
+void drive(long nMotorRatio, long dist, long power)
+{
+	setMotorSyncEncoder(motorB, motorC, nMotorRatio, dist, power);
+}
+
+
+task main()
+{
+	// Use this if you want to use the back button as well
+	// Not advisable.
+	//setBlockBackButton(true);
+	
+	int distance = 0;
+	int randomNum = random(100);
+	
+	displayCenteredTextLine(1, "Pressed button:");
+	displayCenteredBigTextLine(6,"Final Dist = %dcm", distance = getDistance(distance));
+	sleep(1000);
+	drive(0,distance * 36 ,randomNum);
+	sleep(10000);
+}
